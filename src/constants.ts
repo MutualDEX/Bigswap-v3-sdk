3ae51a193d4595b6815b4324b7ba7277176e4d16
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-03-28 12:57:45 -0400

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+import JSBI from 'jsbi'
+
+export declare const FACTORY_ADDRESS = '0xE7Fb3e833eFE5F9c441105EB65Ef8b261266423B'
+
+export declare const INIT_CODE_HASH = '0xf187ed688403aa4f7acfada758d8d53698753b998a3071b06f1b777f4330eaf3'
+
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
